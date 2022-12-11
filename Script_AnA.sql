4de09de7ab7015262def6a1efb337365b09850f7
AnA <100804318+ANILUX3@users.noreply.github.com>
2022-12-11 01:46:22 +0100

    entrega

--- Script_AnA.sql
+++ Script_AnA.sql
@@ -1,4 +1,4 @@
------------------ Script de práctica de módulo SQL -----------------
+----------------- Script de practica de modulo SQL -----------------
 
 --Creamos espacio de trabajo(esquema)
 create schema ana_alonso authorization xzhbycyw;
@@ -7,7 +7,7 @@ create schema ana_alonso authorization xzhbycyw;
 
 --- Grupo empresarial de la marca ---
 create table ana_alonso.brand_groups(
-	id_group varchar(10), not null -- PK
+	id_group varchar(10) not null, -- PK
 	group_name varchar (50) not null,
 	description varchar(512) null
 );
@@ -65,11 +65,11 @@ add constraint colors_PK primary key (id_color);
 --- Vehículos ---
 create table ana_alonso.vehicles(
 	id_vehicle varchar(10) not null, --PK 
-	id_model varchar(10) not null, --PK, FK 
-	id_color varchar(10) not null, --FK -> colors -> id_color
+	id_model varchar(10) not null, --FK1 -> models -> id_model
+	id_color varchar(10) not null, --FK2 -> colors -> id_color
 	plate varchar(50) not null,
 	total_km integer not null,
-	purchase_date date not null,
+	dt_purchase date not null,
 	description varchar(512) null
 );
 
@@ -77,37 +77,88 @@ create table ana_alonso.vehicles(
 alter table ana_alonso.vehicles
 add constraint vehicles_PK primary key (id_vehicle);
 
---FK
+--FKs
 alter table ana_alonso.vehicles
-add constraint vehicles_FK foreign key (id_brand)
-references ana_alonso.brands (id_brand);
+add constraint vehicles_FK1 foreign key (id_model)
+references ana_alonso.models (id_model);
 
-
+alter table ana_alonso.vehicles
+add constraint vehicles_FK2 foreign key (id_color)
+references ana_alonso.colors (id_color);
 
 --- Compañia aseguradora ---
+create table ana_alonso.insurance_companies(
+	id_insurance varchar(10) not null, --PK 
+	insurance_name varchar(50) not null,
+	description varchar(512) null
+);
 
 --PK
+alter table ana_alonso.insurance_companies
+add constraint insurance_companies_PK primary key (id_insurance);
 
---FK
-
---- Revisiones ---
+--- Poliza de vehiculo ---
+create table ana_alonso.vehicle_policies(
+	id_vehicle varchar(10) not null, --PK, FK1 -> vehicles -> id_vehicle 
+	policy_num integer not null, --PK 
+	id_insurance varchar(10) not null, --FK2 ->insurance_companies -> id_insurance
+	dt_hire date not null,
+	dt_termination date not null default '4000-01-01',
+	description varchar(512) null
+);
 
 --PK
+alter table ana_alonso.vehicle_policies
+add constraint vehicle_policies_PK primary key (id_vehicle, policy_num);
 
---FK
+--FKs
+alter table ana_alonso.vehicle_policies
+add constraint vehicle_policies_FK1 foreign key (id_vehicle)
+references ana_alonso.vehicles (id_vehicle);
+
+alter table ana_alonso.vehicle_policies
+add constraint vehicle_policies_FK2 foreign key (id_insurance)
+references ana_alonso.insurance_companies (id_insurance);
 
 --- Monedas ---
+create table ana_alonso.currencies(
+	id_currency varchar(10) not null, --PK 
+	currency_name varchar(50) not null,
+	description varchar(512) null
+);
 
 --PK
+alter table ana_alonso.currencies
+add constraint currencies_PK primary key (id_currency);
 
---FK
+--- Revisiones de vehículo---
+create table ana_alonso.vehicle_inspections(
+	id_vehicle varchar(10) not null, --PK, FK1 -> vehicles -> id_vehicle 
+	inspection_num integer not null, --PK
+	inspection_price integer not null,
+	dt_inspection date not null,
+	id_currency varchar(10) not null default '01', --FK2 -> currencies -> id_currency
+	description varchar(512) null
+);
+
+--PK
+alter table ana_alonso.vehicle_inspections
+add constraint vehicle_inspections_PK primary key (id_vehicle, inspection_num);
+
+--FKs
+alter table ana_alonso.vehicle_inspections
+add constraint vehicle_inspections_FK1 foreign key (id_vehicle)
+references ana_alonso.vehicles (id_vehicle);
+
+alter table ana_alonso.vehicle_inspections
+add constraint vehicle_inspections_FK2 foreign key (id_currency)
+references ana_alonso.currencies (id_currency);
 
 
 
+----------------------------------------------------------------------------------------------------
 
-
-
-
+----- CARGA DE DATOS -----
 
 
 
